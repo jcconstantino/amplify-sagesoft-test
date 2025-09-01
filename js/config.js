90337174300749f28ea9115a9c3264d7b030c3a3
John Chris Pensica Constantino <79827782+jcconstantino@users.noreply.github.com>
2025-09-01 14:39:24 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-1_JV6AsG5fy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '46dcluk1r4gv9cvofhso03o9n5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'agieh1c3mn5cthvan3ul1hq4d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 

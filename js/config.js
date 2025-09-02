8b47c56a4130ea94617f6154f3d57f2a02dab4ec
John Chris Pensica Constantino <79827782+jcconstantino@users.noreply.github.com>
2025-09-02 11:41:48 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_8CGaIT313', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '74mar2vgc7ud2iikjcoqks7vku', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_k8qXcSxt2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '736q78r0kfghdq25ajth5ainc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://fj7jz2wpke.execute-api.ap-southeast-1.amazonaws.com/Amplify2025' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
